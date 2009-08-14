57ca3816b74a27010ff0010971a9982c08dd9cc2
sebasdevelopment@gmx.com <sebasdevelopment@gmx.com@80b09924-88c6-11de-8f56-7b2e0289298d>
2009-08-14 11:45:26 +0000

    Adding irc project files.

---
+++ irc_defender/Defender.cpp
@@ -0,0 +1,9 @@
+/*
+    --------------------------------------------
+    Project:    IRC Defender v1.0
+    Filename:   Defender.cpp
+    Date:       09 August 2009
+    Developers: i386 <baslamers@home.nl>
+    --------------------------------------------
+*/
+
