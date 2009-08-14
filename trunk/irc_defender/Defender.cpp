c8e77a87c0227c3643c8c7760a29f02ba97c18df
sebasdevelopment@gmx.com <sebasdevelopment@gmx.com@80b09924-88c6-11de-8f56-7b2e0289298d>
2009-08-14 12:02:32 +0000

    Deleted old defender.cpp and defender.h

--- trunk/irc_defender/Defender.cpp
+++
@@ -1,9 +0,0 @@
-/*
-    --------------------------------------------
-    Project:    IRC Defender v1.0
-    Filename:   Defender.cpp
-    Date:       09 August 2009
-    Developers: i386 <baslamers@home.nl>
-    --------------------------------------------
-*/
-
