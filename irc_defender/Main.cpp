bea9035243786bf1b8d3370145958d9c9a12c4ef
sebasdevelopment@gmx.com <sebasdevelopment@gmx.com@80b09924-88c6-11de-8f56-7b2e0289298d>
2009-08-17 13:35:41 +0000

    (Linux) Added first scripts

---
+++ irc_defender/Main.cpp
@@ -0,0 +1,27 @@
+/*
+    --------------------------------------------
+    Project:    IRC Defender v1.0
+    Filename:   Main.cpp
+    Date:       09 August 2009
+    Developers: i386 <sebasdevelopment@gmx.com>
+    --------------------------------------------
+*/
+
+int main(int argc, char* argv [])
+{
+    if(argc == 2)
+    {
+        if (strcmp ( argv[1], "--help" ) == 0)
+        {
+            printf ( "Usage: defender [OPTION]\n\n" );
+            printf ( "  --c [FILE]      Alternate defender config file\n" );
+            printf ( "  --help          Shows this.\n" );
+            return 1;
+        }
+    }else
+	if(argc == 0)
+	{
+		printf("Run server..");
+	}
+    return 1;
+}
---
+++ irc_defender/Main.h
@@ -0,0 +1,8 @@
+/*
+    --------------------------------------------
+    Project:    IRC Defender v1.0
+    Filename:   Main.h
+    Date:       09 August 2009
+    Developers: i386 <sebasdevelopment@gmx.com>
+    --------------------------------------------
+*/
